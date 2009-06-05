f59229a867500328c94f683eef17111e2aaeb2cb
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2009-06-04 22:34:45 -0400

    Updated display to make entries index page's form more simplified.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,10 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  layout :determine_layout
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  protected
+  def determine_layout; 'general'; end
 end
