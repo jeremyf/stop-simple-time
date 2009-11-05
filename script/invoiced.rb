4b3e8426bdb1aca474705baa4ac8c186d5c9b91c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2009-11-04 19:47:25 -0500

    Updated invoice script

--- script/invoiced.rb
+++ script/invoiced.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env /Users/jeremyf/Documents/Repositories/git/stop-simple-time/script/runner
 
+puts "Paid Amount:    $#{Entry.amount_to_invoice}"
 puts "Invoice Amount: $#{Entry.not_invoiced.amount_to_invoice}"
 puts "Unpaid Amount:  $#{Entry.not_paid.amount_to_invoice}"
 
 puts "Hours Histogram:"
-Entry.find(:all).group_by {|a| a.spent_on.strftime('%Y-%m')}.inject({}) {|m, (month, coll)| m[month] = coll.sum(&:hours_spent); m}.each do |time, hours|
+Entry.find(:all).group_by {|a| a.spent_on.beginning_of_week.strftime('%Y-%m-%d')}.inject({}) {|m, (month, coll)| m[month] = coll.sum(&:hours_spent); m}.each do |time, hours|
   puts "\t#{time}:\t#{hours}"
 end
