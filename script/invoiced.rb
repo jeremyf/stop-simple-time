868eb7e3979174842d5eee4ca82198a85c0ff847
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2009-11-09 21:45:31 -0500

    Updated script/invoiced.rb to sort histogram

--- script/invoiced.rb
+++ script/invoiced.rb
@@ -5,6 +5,6 @@ puts "Invoice Amount: $#{Entry.not_invoiced.amount_to_invoice}"
 puts "Unpaid Amount:  $#{Entry.not_paid.amount_to_invoice}"
 
 puts "Hours Histogram:"
-Entry.find(:all).group_by {|a| a.spent_on.beginning_of_week.strftime('%Y-%m-%d')}.inject({}) {|m, (month, coll)| m[month] = coll.sum(&:hours_spent); m}.each do |time, hours|
+Entry.find(:all).group_by {|a| a.spent_on.beginning_of_week.strftime('%Y-%m-%d')}.inject({}) {|m, (month, coll)| m[month] = coll.sum(&:hours_spent); m}.sort{|a,b| a[0] <=> b[0]}.each do |time, hours|
   puts "\t#{time}:\t#{hours}"
 end
